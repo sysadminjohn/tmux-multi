aa2174188bc1fbbbba90b9ea4ef824d251aac262
Sysadmin John <61210792+sysadminjohn@users.noreply.github.com>
2023-04-17 11:00:53 +0200

    Add input checks and netcat support. Implement better function separation

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# tmux-multissh
+# tmux-multi
 
-This script spawns a ssh connection to a user provided list of hosts, which can then be found in separate tmux windows
+This script spawns a connection to a user provided list of multiple hosts (ssh or netcat, for instance).
+All the connections will be added to a Tmux session.
+
 
 ## TODO
 
-- Everything
+- TODO itself is yet to be written but the code surely has room for improvement
+
+## Tmux cheat sheet: 
+| Shortcut          |   Description |
+| ------------------|---------------|
+| CTRL+B N          | [N]ext window |
+| CTRL+B P          | [P]revious window |
+| ------------------|---------------|
+| CTRL+B D          | [D]etach |
+| tmux a            | Attach to last session |
+| tmux ls           | List sessions |
+| tmux a -t MY1     | Attach to tmux session named MY1 |
+| ------------------|---------------|
+| CTRL+B C          | New window | 
+| CTRL+B "          | Split vertically | 
+| CTRL+B %          | Split vertically | 
+
+To interact with nested tmuxes on the hosts, use CTRL+B+B
+
 
 
--- tmux-multi.sh
+++ tmux-multi.sh
@@ -1,55 +1,156 @@
 #!/bin/bash
 
 
-TMUX_SESSION_NAME=multissh
+TMUX_SESSION_NAME=ssh
 
 ######### Functions
 
-ssh_connect() { 
-    
-    SSH_USER=$2
-    SSH_PORT=$3
+prepare_connect() {
+
+    which tmux 2>/dev/null 1>/dev/null
+
+    if [[ $(echo $?) -ne 0 ]]; then 
+        echo "tmux package not installed or not found"
+        exit 1 
+    fi
+
+
+
+
+    read -p "TMUX SESSION NAME: " TMUX_SESSION_NAME
+
+    tmux has-session -t "$TMUX_SESSION_NAME" 2>/dev/null
+
+    if [[ $(echo $?) -eq 0 ]]; then
+        echo "Tmux session with that name already exists"
+        exit 1
+    fi
+
+
+
+
 
     read -p "HOSTS: " HOSTS
+
+    if [[ $HOSTS == '' ]]; then 
+        echo "No hosts provided"
+        exit 1
+    fi
+
+
+
+
     
-    tmux new-session -s $TMUX_SESSION_NAME -d 
+    if [[ $1 == 'ssh' ]]; then
+        ssh_connect "$1" "$2" "$3" "$HOSTS"
+    fi
+
+
+    if [[ $1 == 'nc' || $1 == 'netcat' ]]; then
+        nc_connect "$1" "$2" "$HOSTS"
+    fi
+
+}
+
+
+nc_connect() {
+
+    NC_PORT=$2
+    HOSTS=$3
+
+
+    if [[ $NC_PORT -lt 1 || $NC_PORT -gt 65535 ]]; then 
+        echo "Netcat port is invalid"
+        exit 1
+    fi
+    
+
+    tmux new-session -s $TMUX_SESSION_NAME -d "watch -n 5 tmux ls"
 
     for H in $HOSTS ; do 
         i=$((i+1))
-        tmux new-window -t $TMUX_SESSION_NAME:$i -n $H "ssh -p $SSH_PORT $SSH_USER@$H"
-        # tmux new-window -t $TMUX_SESSION_NAME:$i -n $H "ping $H"
+        tmux new-window -t $TMUX_SESSION_NAME:$i -n $H "nc -vv $H $NC_PORT"
     done
 
     tmux attach-session -t $TMUX_SESSION_NAME
 
 }
 
+
+
+ssh_connect() { 
+    
+    SSH_USER=$2
+    SSH_PORT=$3
+    HOSTS=$4
+
+
+
+
+    if [[ $SSH_USER == '' ]]; then 
+        echo "No user provided"
+        exit 1
+    fi
+
+
+
+
+
+    if [[ $SSH_PORT -lt 1 || $SSH_PORT -gt 65535 ]]; then 
+        echo "SSH port is invalid"
+        exit 1
+    fi
+    
+
+
+
+    tmux new-session -s $TMUX_SESSION_NAME -d "watch -n 5 tmux ls"
+
+    for H in $HOSTS ; do 
+        i=$((i+1))
+        tmux new-window -t $TMUX_SESSION_NAME:$i -n $H "ssh -p $SSH_PORT $SSH_USER@$H"
+    done
+
+    tmux attach-session -t $TMUX_SESSION_NAME
+
+}
+
+
+
+
 show_error() {
     echo "Unknown option $1. Use -h or --help for usage"
 }
 
+
+
 show_help() {
 cat << EOF
 Description:
-    Basic script that spawns N ssh connections
+    Basic script that spawns N connections to N hosts with a given command
 
 Usage: 
-    tmux-multi.sh PROTOCOL USERNAME HOSTPORT
+    tmux-multi.sh COMMAND USERNAME HOSTPORT
     
-    Hosts will be asked after pressing ENTER and can be inserted one after the other, in the same line
+    USERNAME is not always required. Hosts will be asked after pressing ENTER and can be inserted one after the other, in the same line
 
 Examples:
     sh tmux-multi.sh ssh myusername 22 
-    (hit ENTER, hosts will be asked at stdin)
+    sh tmux-multi.sh netcat 5666
+    sh tmux-multi.sh nc 5666
 EOF
 }
 
 
+
 ######### Main section
+
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        ssh) ssh_connect "$1" "$2" "$3"; exit 0 ;;
+        ssh) prepare_connect "$1" "$2" "$3"; exit 0 ;;
+        nc) prepare_connect "$1" "$2"; exit 0 ;;
+        netcat) prepare_connect "$1" "$2"; exit 0 ;;
         -h|--help) show_help; exit 0 ;;
         *) show_error "$1"; exit 0 ;;
     esac
